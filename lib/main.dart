6106a15b8cc172a5b5ce3a866c78fc62fe0f7ac0
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-09-23 15:00:47 +0900

    window_sizeを実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kitchen_timer/model/time_display_model.dart';
 import 'package:kitchen_timer/model/timer_model.dart';
 import 'package:kitchen_timer/ui/main_page.dart';
 import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows) {
+    setWindowTitle('ドラえもん');
+    setWindowFrame(const Rect.fromLTWH(10, 100, 1000, 1000));
+  }
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
